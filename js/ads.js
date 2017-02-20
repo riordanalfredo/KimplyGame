13c66000aace263358c4f2609a49893d41364510
riordanalfredo <rdalf2@student.monash.edu>
2017-02-21 09:55:31 +1100

    Initiate

---
+++ js/ads.js
@@ -0,0 +1,69 @@
+
+
+function onDeviceReady() {
+    document.removeEventListener('deviceready', onDeviceReady, false);
+
+    // Set AdMobAds options:
+    admob.setOptions({
+        publisherId:          "ca-app-pub-7538563445332968/6187339537",  // Required
+        interstitialAdId:     "ca-app-pub-7538563445332968/7524471937",  // Optional
+         adSize:               admob.AD_SIZE.SMART_BANNER,
+          bannerAtTop:          false,
+          overlap:              false,
+          offsetStatusBar:      false,
+          isTesting:            false,
+          adExtras :            {},
+          autoShowBanner:       true,
+          autoShowInterstitial: true
+        
+    });
+    
+    admob.createBannerView();
+}
+
+var isPendingInterstitial = false;
+var isAutoshowInterstitial = false;
+
+function prepareInterstitialAd() {
+    if (!isPendingInterstitial) { // We won't ask for another interstitial ad if we already have an available one
+        admob.requestInterstitialAd({
+            autoShowInterstitial: isAutoshowInterstitial
+        });
+    }
+}
+
+function onAdLoadedEvent(e) {
+    if (e.adType === admob.AD_TYPE.INTERSTITIAL && !isAutoshowInterstitial) {
+        isPendingInterstitial = true;
+    }
+}
+
+function onDeviceReady() {
+    document.removeEventListener('deviceready', onDeviceReady, false);
+
+    admob.setOptions({
+       publisherId:          "ca-app-pub-7538563445332968/6187339537",  // Required
+        interstitialAdId:     "ca-app-pub-7538563445332968/7524471937",  // Optional
+    });
+
+    document.addEventListener(admob.events.onAdLoaded, onAdLoadedEvent);
+    prepareIntestitialAd();
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function showInterstitialAd() {
+    if (isPendingInterstitial) {
+        admob.showInterstitialAd(function () {
+                isPendingInterstitial = false;
+                isAutoshowInterstitial = false;
+                prepareInterstitialAd();
+        });
+    } else {
+        // The interstitial is not prepared, so in this case, we want to show the interstitial as soon as possible
+        isAutoshowInterstitial = true;
+        admob.requestInterstitialAd({
+            autoShowInterstitial: isAutoshowInterstitial
+        });
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,45 @@
+function onDeviceReady() {
+    document.removeEventListener('deviceready', onDeviceReady, false);
+
+    // Set AdMobAds options:
+    admob.setOptions({
+        publisherId:          "ca-app-pub-7538563445332968/6187339537",  // Required
+        interstitialAdId:     "ca-app-pub-XXXXXXXXXXXXXXXX/IIIIIIIIII",  // Optional
+        tappxIdiOS:           "/XXXXXXXXX/Pub-XXXX-iOS-IIII",            // Optional
+        tappxIdAndroid:       "/XXXXXXXXX/Pub-XXXX-Android-AAAA",        // Optional
+        tappxShare:           0.5                                        // Optional
+    });
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
